f6836c3e968a95795a3d07eedbc458e2bc1a597b
David <davidacosta25081@yahoo.com>
2021-05-20 16:25:26 -0500

    AD: Adding Class Car variables, constructor and methods.

--- src/com/qa/java/abstraction/Car.java
+++ src/com/qa/java/abstraction/Car.java
@@ -1,5 +1,33 @@
 package com.qa.java.abstraction;
 
-public class Car {
+  public class Car extends Vehicle {
+	  private int numOfTires;
+	  private String size;
+  
+  public Car(String madeIn, int model, int numOfTires, String size) {
+			super(madeIn, model);
+			this.numOfTires = numOfTires;
+			this.size = size;
+  }
+
+  //(Constructor chaining) If NO numOfTires and size is provided, this constructor provides values by default
+    public Car(String madeIn,int model) {
+    this(madeIn,model,4,"medium");
+  }
+
+	@Override
+	public void numberOfTires() {
+		System.out.println("This Car has " + this.numOfTires + " tires!");
+		
+	}
+
+	@Override
+	public void sizeOfVehicle() {
+		System.out.println("This Car has a " + this.size + " size!");
+		
+	}
+
+
+
 
 }
