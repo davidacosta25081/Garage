81bed1b03ef9cceef4384d00e3d9d2b63260ac36
David <davidacosta25081@yahoo.com>
2021-05-20 22:22:17 -0500

    AD: Adding/Completing Bus Class

--- src/com/qa/java/abstraction/Bus.java
+++ src/com/qa/java/abstraction/Bus.java
@@ -1,5 +1,33 @@
 package com.qa.java.abstraction;
 
-public class Bus {
+  public class Bus extends Vehicle {
+	  private int numOfTires;
+	  private String size;
+  
+  public Bus(String madeIn, int model, int numOfTires, String size) {
+			super(madeIn, model);
+			this.numOfTires = numOfTires;
+			this.size = size;
+  }
+
+  //(Constructor chaining) If NO numOfTires and size is provided, this constructor provides values by default
+    public Bus(String madeIn,int model) {
+    this(madeIn,model,8,"Big");
+  }
+
+	@Override
+	public void numberOfTires() {
+		System.out.println("This Bus has " + this.numOfTires + " tires!");
+		
+	}
+
+	@Override
+	public void sizeOfVehicle() {
+		System.out.println("This Bus has a " + this.size + " size!");
+		
+	}
+
+
+
 
 }
